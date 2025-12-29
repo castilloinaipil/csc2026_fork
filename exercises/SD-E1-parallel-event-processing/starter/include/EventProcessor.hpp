0b0a2d3d3040edbd0b0bd6f21a0f1ab3a6326207
inait DevOps <113030883+arturos-inait@users.noreply.github.com>
2025-12-30 00:16:08 +0100

    Add EventProcessor class for event handling

---
+++ exercises/SD-E1-parallel-event-processing/starter/include/EventProcessor.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "Event.hpp"
+#include <vector>
+
+namespace csc2026 {
+
+class EventProcessor {
+public:
+    EventProcessor() = default;
+
+    void reset();
+
+    void processEvent(const Event& event);
+    void processEvents(const std::vector<Event>& events);
+
+    int totalTracks() const { return m_totalTracks; }
+    double totalEnergy() const { return m_totalEnergy; }
+
+    static std::vector<Event> generateSampleEvents(size_t nEvents);
+
+private:
+    int m_totalTracks{0};
+    double m_totalEnergy{0.0};
+};
+
+} // namespace csc2026
+
